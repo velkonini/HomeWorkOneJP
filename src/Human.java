5b6f5335e4c6eb8a9346c7ff35666699b7dbe394
velkonini <avelkov07@gmail.com>
2020-06-14 16:31:09 +0300

    HW1 s

--- src/Human.java
+++ src/Human.java
@@ -1,14 +1,54 @@
-public class Human {
+import java.sql.SQLOutput;
+
+public class Human implements Competitor {
     String name;
+    int maxRunDistance;
+    int maxJumpHeight;
+    boolean atDistance;
 
-    public Human(String name) {
+    public Human(String name, int maxRunDistance, int maxJumpHeight) {
         this.name = name;
-    }
-    void run(){
-        System.out.println(name + " run");
+        this.maxRunDistance = maxRunDistance;
+        this.maxJumpHeight = maxJumpHeight;
+        this.atDistance = true;
     }
 
-    void jump(){
-        System.out.println(name + " jump");
+    @Override
+    public void run(int dist) {
+        if(maxRunDistance == 0){
+            System.out.println(name + " не бегает");
+            atDistance = false;
+            return;
+        }
+        if(dist <= maxRunDistance){
+            System.out.println(name + " успешно пробежала забег");
+        } else {
+            System.out.println(name + " не может столько пробежать");
+            atDistance = false;
+        }
+
     }
+
+    @Override
+    public void jump(int height) {
+        if(maxJumpHeight == 0){
+            System.out.println(name + " не умеет прыгать");
+            atDistance = false;
+            return;
+        }
+        if(height <= maxJumpHeight){
+            System.out.println(name + " успешно перепрыгнул стену");
+        } else {
+            System.out.println(name + " не может перепрыгнуть");
+            atDistance = false;
+        }
+
+    }
+
+    @Override
+    public boolean atDistance() {
+        return atDistance;
+    }
+
+
 }
